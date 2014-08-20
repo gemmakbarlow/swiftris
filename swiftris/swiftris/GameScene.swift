3741bf94cab1cf8ba6310a00dba670402680bc1b
Gemma Barlow <gemmakbarlow@gmail.com>
2014-08-20 11:34:33 -0400

    Initial project
    Some unused code has been removed

---
+++ swiftris/swiftris/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  swiftris
+//
+//  Created by Gemma Barlow on 8/20/14.
+//  Copyright (c) 2014 Gemma Barlow. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+      
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ swiftris/swiftris/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  swiftris
+//
+//  Created by Gemma Barlow on 8/20/14.
+//  Copyright (c) 2014 Gemma Barlow. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
